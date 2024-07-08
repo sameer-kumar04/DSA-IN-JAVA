2f82d4d11f65e41af96f9e61e8f288286a76a306
sameer-kumar04 <167437007+sameer-kumar04@users.noreply.github.com>
2024-07-08 19:33:37 +0530

    Add files via upload

---
+++ DissapearednumLC448.java
@@ -0,0 +1,41 @@
+package SortingAlgo;
+
+import java.util.*;
+
+public class DissapearednumLC448 {
+    public static void main(String[] args) {
+
+    }
+
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+
+        int i = 0;
+
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[correct] != nums[i]) {
+                swap(nums, i, correct);
+            }
+
+            else {
+                i++;
+            }
+        }
+        // now here we have to find missing numbers
+        List<Integer> ans = new ArrayList<Integer>();
+
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(nums[index + 1]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int last) {
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+    }
+}
