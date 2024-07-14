92ffa9e697098768d24d79b7e05646148e1b0054
sameer-kumar04 <whoissameer04@gmail.com>
2024-07-14 07:27:56 +0530

    Bit Manipulation

--- Findunique.java
+++ Findunique.java
@@ -2,7 +2,7 @@ package JAVADSAREPO;
 
 public class Findunique {
     public static void main(String[] args) {
-        int[] array = { 2, 4, 2, 6, 4 };
+        int[] array = { 2, 4, 2, 6, 4 ,2,2,4,4};
 
         System.out.println(find(array));
     }
