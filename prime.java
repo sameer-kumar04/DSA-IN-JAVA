8175e383b07bf42df5c32150a126820e10976d3d
sameer-kumar04 <whoissameer04@gmail.com>
2024-07-15 15:06:21 +0530

    Prime Number

--- prime.java
+++ prime.java
@@ -5,29 +5,26 @@ import java.util.Scanner;
 public class prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter any Number : ");
+        System.out.print("Enter a Number : ");
         int n = sc.nextInt();
-        boolean ans = isPrime(n);
-        System.out.println(ans);
+        System.out.println(isPrime(n));
     }
+
     static boolean isPrime(int n) {
-        if (n<=1) {
+        if (n <= 1) {
             return false;
         }
-        if (n==2) {
+        if (n == 2) {
             return true;
         }
-        if ((n & 1)==0) { //if number is even then it will not a prime number
+        if ((n & 1) == 0) { // if number is even then it will not a prime number
             return false;
         }
-        for (int i = 3; i<=Math.sqrt(n); i++) {
-            if ( n % i == 0) {
+        for (int i = 3; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 return false;
             }
-            else {
-                return true;
-            }
         }
-        return false;
+        return true;
     }
 }
