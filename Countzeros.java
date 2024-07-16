310c805ac6b35e718c183c0e95cc620ce4b5cd1f
sameer-kumar04 <whoissameer04@gmail.com>
2024-07-16 16:48:57 +0530

    Count number occurance using Recursion

--- Countzeros.java
+++ Countzeros.java
@@ -8,18 +8,33 @@ public class Countzeros {
         System.out.print("Enter a Number : ");
         int n = sc.nextInt();
 
-        System.out.println(countzero(n, 0));
+        System.out.println(countzero(n));
     }
 
-    public static int countzero(int n, int c) {
-        if (n == 0) {
-            return c;
-        }
-        int rem = n % 10;
-        if (rem == 0) {
-            return countzero(n / 10, c + 1);
+    // public static int countzero(int n, int c) {
+    // if (n == 0) {
+    // return c;
+    // }
+    // int rem = n % 10;
+    // if (rem == 0) {
+    // return countzero(n / 10, c + 1);
+    // }
+
+    // return countzero(n / 10, c);
+    // }
+
+    static int countzero(int num) {
+        return helper(num, 0);
+    }
+
+    static int helper(int num, int count) {
+        if (num == 0) {
+            return count;
         }
 
-        return countzero(n / 10, c);
+        if (num % 10 == 0) {
+            return helper(num / 10, count + 1);
+        }
+        return helper(num / 10, count);
     }
 }
