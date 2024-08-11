f8222851103f0feb896aa1cf0727d6d64bcbe364
sameer-kumar04 <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-08-11 08:23:50 +0530

    Using recursion helper function

---
+++ Issorted.java
@@ -0,0 +1,21 @@
+package Earlier;
+
+public class Issorted {
+    public static void main(String[] args) {
+        int[] array = {1,3,4,2,5,6};
+        System.out.println(issorted(array));
+    }
+
+    static boolean issorted(int[] arr) {
+        return helper(arr,0);
+    }
+
+    static boolean helper (int[] arr, int index) {
+
+        if (index == arr.length - 1) {
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && helper(arr,index + 1);
+    }
+}
