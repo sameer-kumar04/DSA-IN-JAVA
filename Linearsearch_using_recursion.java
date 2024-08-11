c2085f416a2facef8d6afe3cb1e43b3b42c43e80
sameer-kumar04 <112376748+IamSudhir-Kumar@users.noreply.github.com>
2024-08-11 10:59:06 +0530

    Linear search using recursion

---
+++ Linearsearch_using_recursion.java
@@ -0,0 +1,20 @@
+package Earlier;
+
+public class Linearsearch_using_recursion {
+    public static void main(String[] args) {
+        int[] array = {2,4,6,8,10};
+        System.out.println(linearsearch(array,8,0));
+    }
+
+    static int linearsearch(int[] arr, int target, int index) {
+        if ( index > arr.length - 1) {
+            return -1; //target not found
+        }
+
+        if ( arr[index] == target) {
+            return index;
+        }
+
+        return linearsearch(arr,target,index+1);
+    }
+}
